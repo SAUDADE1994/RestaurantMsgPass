cee2001d04eccb5a6a3d2208415c2a3f9da7c0b9
Andre Almeida <aralmeida99@gmail.com>
2022-05-21 17:38:24 +0100

    Minor fixes to scripts
    Distraction errors and such writing the scripts

--- src/KitchenDeployAndRun.sh
+++ src/KitchenDeployAndRun.sh
@@ -4,6 +4,6 @@ DIRNAME='dirKitchen'
 DESCRIBE='kitchen'
 MAIN='KitchenMain'
 PORT='22382'
-USER="sd209@1040101-ws03.ua.pt"
+USER="sd209@l040101-ws03.ua.pt"
 
 source NodeDeployAndRun.sh "$DIRNAME" "$DESCRIBE" "$MAIN" "$PORT" "$USER"
--- src/TableDeployAndRun.sh
+++ src/TableDeployAndRun.sh
@@ -4,6 +4,6 @@ DIRNAME='dirTable'
 DESCRIBE='table'
 MAIN='TableMain'
 PORT='22380'
-USER="sd209@1040101-ws01.ua.pt"
+USER="sd209@l040101-ws01.ua.pt"
 
 source NodeDeployAndRun.sh "$DIRNAME" "$DESCRIBE" "$MAIN" "$PORT" "$USER"
